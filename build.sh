23d77ac9df49202d90602a9c1f57403f46c54813
Mark <mark@upstarters.com>
2025-01-16 22:05:41 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,16 @@
 
 set -ouex pipefail
 
+# VSCode because it's still better for a lot of things
+tee /etc/yum.repos.d/vscode.repo <<'EOF'
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
