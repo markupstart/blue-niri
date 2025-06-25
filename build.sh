f715fb19452ab2a6639c7cb65293cdc7f953dd6b
Mark <mark@upstarters.com>
2025-06-25 16:14:01 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,16 +2,6 @@
 
 set -ouex pipefail
 
-# VSCode because it's still better for a lot of things
-tee /etc/yum.repos.d/vscode.repo <<'EOF'
-[code]
-name=Visual Studio Code
-baseurl=https://packages.microsoft.com/yumrepos/vscode
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-EOF
-
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -22,115 +12,25 @@ EOF
 #  install packages from fedora repos
 dnf install -y dnf5-plugins \
 nextcloud-client \
-btop \
-htop \
-flatpak \
-micro \
-fish \
-cascadia-code-fonts \
-wofi \
-edk2-ovmf \
-google-droid-sans-mono-fonts \
-mozilla-fira-mono-fonts \
-google-noto-emoji-fonts \
-osbuild-selinux \
-p7zip-plugins \
-p7zip \
-zoxide \
-fish \
-atuin \
-copyq \
-swayimg \
-qemu \
-ydotool \
-fastfetch \
-chafa \
-code \
-just \
-rocminfo \
-rocm-clinfo \
-rocm-hip \
-rocm-opencl \
-rocm-device-libs \
-llvm \
 darktable \
-distrobox \
-podman \
-firefox \
-fedora-repos-ostree \
-gamemode \
-ghostscript \
-glibc-all-langpacks \
-gnome-boxes \
-mangohud \
-google-noto-color-emoji-fonts \
-google-noto-emoji-fonts \
-google-noto-sans-cjk-fonts \
-grub2-efi-ia32 \
-grub2-tools-extra \
-langpacks-core-en \
-langpacks-en \
-langpacks-fonts-en \
-langtable \
-xcb-util-cursor \
-xcb-util \
-xorg-x11-server-Xwayland \
-xdg-user-dirs \
-xdg-utils \
-pipewire-utils \
-zram-generator-defaults \
-zip \
-wl-clipboard \
-wget2 \
-samba-client \
-plymouth \
-plymouth-system-theme \
-plymouth-theme-spinner \
-ostree-grub2 \
-ristretto \
-cups \
-cups-filters \
-cups-browsed \
-mousepad \
-NetworkManager \
-dhcp-client \
-ghostscript \
+wofi \
 mate-polkit \
 mako \
-waybar \
-swaybg \
-swayidle \
-swaylock \
-pavucontrol \
 thunar \
 thunar-volman \
 thunar-media-tags-plugin \
 thunar-vcs-plugin \
 thunar-archive-plugin \
-gvfs-fuse \
-gvfs-nfs \
-gvfs-smb \
-gvfs-gphoto2 \
-gvfs-goa \
-gvfs-afp \
-engrampa \
 tumbler \
-xdg-desktop-portal-gtk \
-xdg-desktop-portal-gnome \
-ptyxis \
-apr \
-apr-util \
-mesa-libGLU \
-libxcrypt-compat \
-wine \
-winetricks \
-curl \
-zenity \
-vulkan-tools \
-vulkan-loader
-
-#testing GNOME Desktop adding thru dnf5
-dnf5 -y install gdm --setopt=install_weak_deps=False
+copyq \
+swaybg \
+swaylock \
+swayidle \
+pavucontrol \
+waybar \
+cargo \
+pamixer \
+libreoffice \
 
 #  COPR:
 #niri
@@ -153,46 +53,20 @@ dnf5 -y copr enable xeriab/ghostty
 dnf5 -y install ghostty
 # Disable COPRs so they don't end up enabled on the final image:
 dnf5 -y copr disable xeriab/ghostty
-#for starship
-dnf5 -y copr enable markupstart/terminal-stuff
-dnf5 -y install starship
+#for swww
+dnf5 -y copr enable lebastr/sway-extras
+dnf5 -y install swww
 # Disable COPRs so they don't end up enabled on the final image:
-dnf5 -y copr disable markupstart/terminal-stuff
-# use negativo17 for 3rd party packages with higher priority than default
-if ! grep -q fedora-multimedia <(dnf5 repolist); then
-    # Enable or Install Repofile
-    dnf5 config-manager setopt fedora-multimedia.enabled=1 ||
-        dnf5 config-manager addrepo --from-repofile="https://negativo17.org/repos/fedora-multimedia.repo"
-fi
-# Set higher priority
-dnf5 config-manager setopt fedora-multimedia.priority=90
+dnf5 -y copr disable lebastr/sway-extras
 
-# use override to replace mesa and others with less crippled versions
-OVERRIDES=(
-    "libva"
-    "mesa-dri-drivers"
-    "mesa-filesystem"
-    "mesa-libEGL"
-    "mesa-libGL"
-    "mesa-libgbm"
-    "mesa-va-drivers"
-    "mesa-vulkan-drivers"
-)
-
-dnf5 distro-sync -y --repo='fedora-multimedia' "${OVERRIDES[@]}"
-dnf5 versionlock add "${OVERRIDES[@]}"
-
-#### Example for enabling a System Unit File
-
+#for cosmic desktop
+dnf5 -y copr enable ryanabx/cosmic-epoch
+dnf5 -y install cosmic-desktop
+# Disable COPRs so they don't end up enabled on the final image:
+dnf5 -y copr disable ryanabx/cosmic-epoch
+ 
 #change pretty name
 sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"blue-niri (FROM Fedora Linux $(rpm -E %fedora))\"|" /usr/lib/os-release
-
-#disable vscode repo, so it's not enabled on the final system
-sed -i 's@enabled=1@enabled=0@g' "/etc/yum.repos.d/vscode.repo"
-sed -i 's@enabled=1@enabled=0@g' "/etc/yum.repos.d/fedora-multimedia.repo"
-
-# Remove dnf5 versionlocks
-dnf5 versionlock clear
 
 # Cleanup
 # Remove tmp files and everything in dirs that make bootc unhappy
